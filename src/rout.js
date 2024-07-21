7cd53cd7acd98c2a39319ffbe7f1b686e5ebb8f9
MohamedAboB <158477987+MohamedAboB@users.noreply.github.com>
2024-07-21 12:52:43 +0300

    Update rout.js

--- src/rout.js
+++ src/rout.js
@@ -2,12 +2,15 @@ import React from 'react'
 import {Routes, Route} from 'react-router-dom'
 import Signup from './Signup'
 import Login from './Login'
+import Welcome from './Welcome';
 const Rout = () => {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Signup />}></Route>
-        <Route path='/Login' element={<Login/>}></Route>
+      <Route path="/signup" element={<Signup />} />
+                <Route path="/welcome" element={<Welcome />} />
+                <Route path="/login" element={<Login />} /> {/* Assuming you have a Login component */}
+                <Route path="/" element={<Signup />} /> {/* Redirect to Signup or another default page */}
       </Routes>
     </div>
   )
